d60b5a2afa91730d026ed4786fd8388a717fc69a
Ryan Turaids <ryturaids@gmail.com>
2024-06-30 18:06:38 -0700

    finished assignment

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { puppyList } from './data.js'
+import './App.css'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  function setFeaturedPup() {
+
+  }
+  return (
+    <>
+      <div>
+        <section>
+          {
+            puppies.map((puppy) => {
+                return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+              })
+          }
+        </section>
+        { featPupId && (
+          <div className='featured-puppy'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        ) } 
+      </div>
+    </>
+  )
+}
+
+export default App
